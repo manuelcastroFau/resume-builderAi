1b1e332456768c5da2b66c8ddff3706d1b626441
Manuel Castro <manuelcastro2021@fau.edu>
2023-05-14 02:31:37 -0400

    Add openAi package

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const bodyParser = require("body-parser");
+
 const app = express()
+app.use(express.static(__dirname + "/public"));
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+    apiKey: process.env.OPEN_AI_API_KEY,
+  });
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.sendFile("/index.html");
 })
 
 app.get('/manuel', function (req, res) {
